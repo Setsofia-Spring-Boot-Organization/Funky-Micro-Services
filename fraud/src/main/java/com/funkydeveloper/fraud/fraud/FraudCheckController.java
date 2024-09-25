a560499d7a487c540d9c76d13b465e1c4383575d
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-09-25 22:29:01 +0000

    Update: check for fraudulent customer was added

---
+++ fraud/src/main/java/com/funkydeveloper/fraud/fraud/FraudCheckController.java
@@ -0,0 +1,22 @@
+package com.funkydeveloper.fraud.fraud;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/fraud")
+@RequiredArgsConstructor
+public class FraudCheckController {
+
+    private final FraudCheckService fraudCheckService;
+
+    @GetMapping(path = "{customerId}")
+    public boolean checkFraudulentCustomer(
+            @PathVariable Integer customerId
+    ) {
+        return fraudCheckService.isFraudulentCustomer(customerId);
+    }
+}
