4a23345f2032437696e9c16b5365f06807672ba1
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-09-26 14:18:58 +0000

    Update: fraudulent customer detection added from the customer service by first checking the users' id from the FraudCheckController

--- customer/src/main/java/com/funkydeveloper/customer/customer/CustomerService.java
+++ customer/src/main/java/com/funkydeveloper/customer/customer/CustomerService.java
@@ -1,9 +1,15 @@
 package com.funkydeveloper.customer.customer;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@RequiredArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void creatNewCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -12,6 +18,16 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .email(request.email())
                 .build();
 
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+
+        boolean isFraudulentCustomer = Boolean.TRUE.equals(restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{{customerId}}",
+                Boolean.class,
+                customer.getId()
+        ));
+
+        if (isFraudulentCustomer) {
+            throw new IllegalStateException("user is a fraud");
+        }
     }
 }
